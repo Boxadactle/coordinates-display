728a9d8a5056c7c02d927ba7f6e554006d6777fa
Boxadactle <109389081+Boxadactle@users.noreply.github.com>
2023-09-24 11:07:18 -0700

    Try doing this

--- common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
+++ common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
@@ -17,7 +17,7 @@ public class CoordinatesDisplay {
 
 	public static final String MOD_ID = "coordinatesdisplay";
 
-	public static final String VERSION = "4.0.0";
+	public static final String VERSION = "${version}";
 
 	public static final String VERSION_STRING = MOD_NAME + " v" + VERSION;
 
