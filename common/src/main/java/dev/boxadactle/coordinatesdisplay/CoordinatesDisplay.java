213e00cb92006e2081f04b62e5ace7b71250da5b
Boxadactle <boxadactle@boxadactle.dev>
2024-06-30 21:43:16 -0700

    bump version

--- common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
+++ common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
@@ -23,7 +23,7 @@ public class CoordinatesDisplay {
 
 	public static final String MOD_ID = "coordinatesdisplay";
 
-	public static final String VERSION = "10.0.0";
+	public static final String VERSION = "11.0.1";
 
 	public static final String VERSION_STRING = MOD_NAME + " v" + VERSION;
 
