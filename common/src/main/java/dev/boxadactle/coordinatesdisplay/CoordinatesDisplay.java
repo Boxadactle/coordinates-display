6e522dcaafa33c31fa63b816d63faaa218c4190b
Boxadactle <boxadactle@boxadactle.dev>
2024-08-17 20:18:36 -0700

    compile with correct 1.17 version

--- common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
+++ common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
@@ -24,7 +24,7 @@ public class CoordinatesDisplay {
 
 	public static final String MOD_ID = "coordinatesdisplay";
 
-	public static final String VERSION = "4.1.0";
+	public static final String VERSION = "4.1.1";
 
 	public static final String VERSION_STRING = MOD_NAME + " v" + VERSION;
 
