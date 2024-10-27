cd76e021a7d64c0ef76e52e8a06d007992818435
Boxadactle <boxadactle@boxadactle.dev>
2024-10-27 15:03:54 -0700

    add compass rendering

--- common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
+++ common/src/main/java/dev/boxadactle/coordinatesdisplay/CoordinatesDisplay.java
@@ -14,7 +14,7 @@ public class CoordinatesDisplay {
 
 	public static final String MOD_ID = "coordinatesdisplay";
 
-	public static final String VERSION = "13.1.0";
+	public static final String VERSION = "14.0.0";
 
 	public static final String VERSION_STRING = MOD_NAME + " v" + VERSION;
 
